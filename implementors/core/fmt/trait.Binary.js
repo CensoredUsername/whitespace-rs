bd86bf2523a8a6fb28b29be375ff57ea3536f1b9
CensoredUsername <cens.username@gmail.com>
2017-05-30 17:25:03 +0200

    Rebuild docs

---
+++ implementors/core/fmt/trait.Binary.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["whitespacers"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Binary.html\" title=\"trait core::fmt::Binary\">Binary</a> for <a class=\"struct\" href=\"whitespacers/struct.Options.html\" title=\"struct whitespacers::Options\">Options</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
